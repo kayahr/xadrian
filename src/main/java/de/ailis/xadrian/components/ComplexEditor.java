cb2c00bd570d02e327b2fdd70f0e69cd719cf297
Klaus Reimer <k@ailis.de>
2009-03-08 15:22:20 +0000

    Fixed popup menu problem (Checkbox were not initilized correctly when loading a complex)

--- src/main/java/de/ailis/xadrian/components/ComplexEditor.java
+++ src/main/java/de/ailis/xadrian/components/ComplexEditor.java
@@ -142,6 +142,8 @@ public class ComplexEditor extends JComponent implements HyperlinkListener,
 
         // Redraw the content
         redraw();
+        
+        this.fireComplexState();
     }
 
 
