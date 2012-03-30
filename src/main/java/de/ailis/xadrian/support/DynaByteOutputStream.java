b1cfce22b68b7d74f96518d739f2a30e60dc592c
Klaus Reimer <k@ailis.de>
2012-03-30 13:09:54 +0200

    Fixed warning.

--- src/main/java/de/ailis/xadrian/support/DynaByteOutputStream.java
+++ src/main/java/de/ailis/xadrian/support/DynaByteOutputStream.java
@@ -10,21 +10,21 @@ import java.io.OutputStream;
 
 /**
  * Output stream which writes dynamically sized integers. DynaByte streams
- * contains integer values which are formed by a dynamic number of bytes. 
- * Only the first 7 bits of each byte is part of the integer value. The 
- * highest bit of each byte defines if there are more bytes to add to the 
+ * contains integer values which are formed by a dynamic number of bytes.
+ * Only the first 7 bits of each byte is part of the integer value. The
+ * highest bit of each byte defines if there are more bytes to add to the
  * current integer.
- * 
+ *
  * @author Klaus Reimer (k@ailis.de)
  */
 public class DynaByteOutputStream extends OutputStream
 {
     /** The original output stream. */
-    private OutputStream stream;
+    private final OutputStream stream;
 
     /**
      * Constructor.
-     * 
+     *
      * @param stream
      *            The original output stream.
      */
@@ -37,16 +37,17 @@ public class DynaByteOutputStream extends OutputStream
      * @see java.io.OutputStream#write(int)
      */
     @Override
-    public void write(int b) throws IOException
+    public void write(final int b) throws IOException
     {
+        int nextB = b;
         do
         {
-            int cur = b & 127;
-            b >>>= 7;
+            final int cur = nextB & 127;
+            nextB >>>= 7;
             int v = cur;
-            if (b > 0) v |= 128;
+            if (nextB > 0) v |= 128;
             this.stream.write(v);
         }
-        while (b > 0);
+        while (nextB > 0);
     }
 }
